fe923050ffb227b28b9b38f2afdd4811a547fde1
joohhhnnnny <johnbongcacjohn@gmail.com>
2025-04-09 20:42:35 +0800

    small changes forda char, prep tomorrow

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,11 +6,11 @@ export const Navbar = ({menuOpen, setMenuOpen}) => {
     }, [menuOpen]);
 
   return (
-    <nav className="fixed top-0 w-full z-40 bg-[rgba(10, 10, 10, 0.8)] backdrop-blur-lg border-b border-white shadow-lg">
+    <nav className="fixed top-0 w-full z-40 bg-[rgba(10, 10, 10, 0.8)] backdrop-blur-lg border-b border-emerald-300 shadow-lg">
       <div className="max-w-5xl m-auto px-4">
         <div className="flex justify-between items-center h-16">
           <a href="#home" className="font-mono text-xl font-bold text-white relative z-30"> 
-            pedro <span className="text-blue-500">.tech</span> 
+            Joohhhnnnn<span className="text-emerald-300">_v</span> 
           </a>
 
           <div className={`w-7 h-5 relative cursor-pointer z-30 md:hidden transition-opacity duration-300
