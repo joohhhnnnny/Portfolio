a2bc08e98423b691f44ae0b740992ef8b9b125e8
joohhhnnnny <johnbongcacjohn@gmail.com>
2025-04-30 10:56:23 +0800

    balik nasaaaad

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -5,7 +5,16 @@ export const Projects = () => {
       className="main-h-screen flex items-center justify-center py-20"
     >
       <div className="max-w-5xl mx-auto px-4">
-        <h2>Featured PRojects</h2>
+        <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-purple-500 to-blue-400 bg-clip-text text-transparent text-center">
+          Featured Projects
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+
+          <div>
+            <h3> Cloud Platform </h3>
+          </div>
+
+        </div>
       </div>
     </section>
   );
